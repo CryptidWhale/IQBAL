c58211812f10f4020825d570a7739f01f5bb5d64
CryptidWhale <156840605+CryptidWhale@users.noreply.github.com>
2024-02-21 01:07:32 +0800

    Add files via upload

---
+++ LeScript.js
@@ -0,0 +1,9 @@
+function button_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("mySidebar").style.width = "30%";
+}
+
+function button_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("mySidebar").style.width = "0";
+}
